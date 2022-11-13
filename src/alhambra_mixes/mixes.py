b2cee70e60865a11cb5e6715855e7a26bed805a1
David Doty <doty@ucdavis.edu>
2022-11-13 09:52:30 -0800

    black

--- src/alhambra_mixes/mixes.py
+++ src/alhambra_mixes/mixes.py
@@ -1393,10 +1393,12 @@ def compute_shared_actions(
             at_least_one_unique_action = True
         unique_action_lists.append(unique_actions)
     if not at_least_one_unique_action:
-        raise ValueError('None of the mixes has any actions unique to it, so it does not make sense '
-                         'to create a master mix.\nSee the function `split_mix` for a simpler function '
-                         'that achieves the goal of making a large mix that can be split into identical '
-                         'test tubes.')
+        raise ValueError(
+            "None of the mixes has any actions unique to it, so it does not make sense "
+            "to create a master mix.\nSee the function `split_mix` for a simpler function "
+            "that achieves the goal of making a large mix that can be split into identical "
+            "test tubes."
+        )
 
     return shared_actions, unique_action_lists
 
